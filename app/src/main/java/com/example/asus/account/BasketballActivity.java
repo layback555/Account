dd433e4aca0683cf9188073983732b98e624ad23
layback555 <1492379769@qq.com>
2019-04-02 20:31:03 +0800

    Initial commit

---
+++ app/src/main/java/com/example/asus/account/BasketballActivity.java
@@ -0,0 +1,37 @@
+package com.example.asus.account;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+public class BasketballActivity extends AppCompatActivity {
+  TextView score;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_basketball);
+        score = (TextView)findViewById(R.id.textView2);
+
+    }
+    public void btnAdd1(View btn){
+        showScore(1);
+    }
+    public void btnAdd2(View btn){
+        showScore(2);
+    }
+    public void btnAdd3(View btn){
+        showScore(3);
+    }
+    public void btnReset(View btn){
+        score.setText("0");
+    }
+    private void showScore(int inc){
+        Log.i("show","inc="+inc);
+        String oldScore=(String) score.getText();
+        int newScore=Integer.parseInt(oldScore)+inc;
+        score.setText(""+newScore);
+    }
+
+}
