8a69b5d1d260fb95d5e1ef3e96560ba4c5ec169a
layback555 <1492379769@qq.com>
2019-04-02 23:30:03 +0800

    basketball

--- app/src/main/java/com/example/asus/account/BasketballActivity.java
+++ app/src/main/java/com/example/asus/account/BasketballActivity.java
@@ -7,15 +7,13 @@ import android.view.View;
 import android.widget.TextView;
 
 public class BasketballActivity extends AppCompatActivity {
-  TextView score,score1;
+  TextView score;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_basketball);
         score = (TextView)findViewById(R.id.textView2);
-        score1 = (TextView)findViewById(R.id.textView4);
         score.setText("0");
-        score1.setText("0");
     }
 
      public void btnAdd1(View btn){
@@ -27,18 +25,8 @@ public class BasketballActivity extends AppCompatActivity {
     public void btnAdd3(View btn){
         showScore(3);
     }
-    public void btnAdd11(View btn){
-        showScore1(1);
-    }
-    public void btnAdd22(View btn){
-        showScore1(2);
-    }
-    public void btnAdd33(View btn){
-        showScore1(3);
-    }
     public void btnReset(View btn){
         score.setText("0");
-        score1.setText("0");
     }
 
     private void showScore(int inc){
@@ -46,11 +34,6 @@ public class BasketballActivity extends AppCompatActivity {
         String oldScore=(String) score.getText();
         int newScore=Integer.parseInt(oldScore)+inc;
         score.setText(""+newScore);
-    }
-    private void showScore1(int inc1){
-        Log.i("show","inc="+inc1);
-        String oldScore1=(String) score1.getText();
-        int newScore1=Integer.parseInt(oldScore1)+inc1;
-        score1.setText(""+newScore1);
+
     }
 }
