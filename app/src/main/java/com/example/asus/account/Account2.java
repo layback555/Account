34957f75b5bfd3c662d38cca3b0f77bbb9cc4d57
layback555 <1492379769@qq.com>
2019-04-02 22:43:57 +0800

    Two teams competition

---
+++ app/src/main/java/com/example/asus/account/Account2.java
@@ -0,0 +1,13 @@
+package com.example.asus.account;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Account2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account2);
+    }
+}
